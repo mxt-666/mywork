011c3cdfb39d77bba0a77c2e8f7814ceeefd362a
mxt-666 <2982755112@qq.com>
2021-07-22 20:13:33 +0800

    process_copy

--- copy.c
+++ copy.c
@@ -1,36 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<dirent.h>
 #include<string.h>
 #include<sys/stat.h>
+#include<sys/mman.h>
 #include<sys/types.h>
-#include<unistd.h>
 #include<fcntl.h>
+#include<unistd.h>
 
-
-int main(int argc,char *argv[])
+int main(int argc,char* argv[])
 {
-	char *copyby = argv[1];
-	char *copyto = argv[2];
-	char *size = argv[3];
-	char *begin = argv[4];
+	char* copyby = argv[1];
+	char* copyto = argv[2];
+	char* copy_size = argv[3];
+	char* copy_begin = argv[4];
 
-	int fwd1,fwd2;
-	int len = atoi(size);
-	int pbegin = atoi(begin);
+	int size = atoi(copy_size);
+	int begin = atoi(copy_begin);
+	int fwd1 = open(copyby,O_RDWR);
+	int fwd2 = open(copyto,O_RDWR|O_CREAT,0664);
 
 	char buffer[4096];
 	memset(buffer,0,sizeof(buffer));
 
-	fwd1 = open(copyby,O_RDWR);
-	fwd2 = open(copyto,O_RDWR|O_CREAT,0664);
-	
-	lseek(fwd1,pbegin,SEEK_SET);
-	if((len = read(fwd1,buffer,len)) >= 0)
-	{
-		lseek(fwd2,pbegin,SEEK_SET);
-		write(fwd2,buffer,len);
-	}
+	lseek(fwd1,begin,SEEK_SET);
+	size = read(fwd1,buffer,size);
+
+	lseek(fwd2,begin,SEEK_SET);
+	write(fwd2,buffer,size);
 
 	return 0;
 }
--- process_copy.c
+++ process_copy.c
@@ -1,27 +1,11 @@
-#include<stdio.h>
 #include<stdlib.h>
+#include<stdio.h>
 #include<string.h>
-#include<unistd.h>
-#include<fcntl.h>
 #include<sys/stat.h>
-#include<sys/mman.h>
 #include<sys/types.h>
+#include<fcntl.h>
+#include<unistd.h>
 #include<sys/wait.h>
-
-int get_file_size(char* copyby)
-{
-	int filesize = -1;
-	struct stat statbuff;
-	if(stat(copyby,&statbuff) < 0)
-	{
-		return filesize;
-	}
-	else
-	{
-		filesize = statbuff.st_size;
-	}
-	return filesize;
-}
 
 int main(int argc,char* argv[])
 {
@@ -29,46 +13,42 @@ int main(int argc,char* argv[])
 	char* copyto = argv[2];
 	char* process_num = argv[3];
 
-	int num = 10;
-	if(argv[3]!=0)
+	int process = 10;
+	if(process_num != NULL)
 	{
-		num = atoi(process_num);
+		process = atoi(process_num);
+		if(process > 100||process < 0)
+		{
+			printf("进程数目输入错误！\n");
+			exit(1);
+		}
 	}
-	//int sizemax = get_file_size(copyby);
 	
-	int fd;
+	int sizemax,size,fd;
 	if((fd = open(copyby,O_RDWR)) == -1)
 	{
-		printf("复制文件无\n");
-		return 0;
+		printf("复制文件不存在!\n");
+		exit(2);
 	}
 
-	int sizemax;
-	int size;
-
-	if(num > 100||num <= 0)
-	{
-		printf("进程数输入错误\n");
-		return 0;
-	}
 	if((sizemax = lseek(fd,0,SEEK_END)) == -1)
 	{
 		printf("复制文件为空！\n");
-		return 0;
+		exit(3);
 	}
 
-	if(sizemax % num == 0)
+	if(sizemax%process != 0)
 	{
-		size = sizemax/num;
+		size = sizemax/process + 1;
 	}
 	else
 	{
-		size = sizemax/num + 1;
+		size = sizemax/process;
 	}
 
-	pid_t pid;
 	int i;
-	for(i = 0;i < num;i++)
+	pid_t pid;
+	for(i = 0;i < process;i++)
 	{
 		pid = fork();
 
@@ -76,27 +56,18 @@ int main(int argc,char* argv[])
 			break;
 	}
 
-	int begin = i*size;
-	char str_begin[4096];
-	sprintf(str_begin,"%d",begin);
-	char str_size[4096];
-	sprintf(str_size,"%d",size);
-
 	if(pid > 0)
 	{
-		//execlp("copy","copy",copyby,copyto,str_size,str_begin,NULL);
-
+		int num = 0;
 		pid_t wpid;
-		int die = 0;
 		while((wpid = waitpid(-1,NULL,WNOHANG)) != -1)
 		{
 			if(wpid > 0)
 			{
-				die++;
-
-				if(die == num)
+				num++;
+				if(num == process)
 				{
-					printf("复制结束！\n");
+					printf("复制成功！\n");
 					return 0;
 				}
 			}
@@ -104,15 +75,21 @@ int main(int argc,char* argv[])
 	}
 	else if(pid == 0)
 	{
-		printf("copyby :%s,copyto :%s,str_size :%s,str_begin :%s\n",copyby,copyto,str_size,str_begin);
-		execl("/home/colin/colin第五阶段/mywork/copy","copy",copyby,copyto,str_size,str_begin,NULL);
-		exit(1);
+		char copy_size[4096];
+		char copy_begin[100];
+		int begin = i*size;
+
+		sprintf(copy_size,"%d",size);
+		sprintf(copy_begin,"%d",begin);
+		
+		printf("copyby:%s copyto:%s size:%s begin:%s\n",copyby,copyto,copy_size,copy_begin);
+		execlp("/home/colin/colin第五阶段/mywork/copy","copy",copyby,copyto,copy_size,copy_begin,NULL);
 	}
 	else
 	{
-		printf("process woring....\n");
+		printf("进程创建错误!\n");
 		return 0;
 	}
-	return 0;
 
+	return 0;
 }
